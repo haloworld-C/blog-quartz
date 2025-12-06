5b384f16956abcdcc0adc604c6246d1cd94c39fb
hal <guangaltman@163.com>
2025-12-06 19:58:32 +0800

    add git action

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "haloworld-C.github.io/blog-quartz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
